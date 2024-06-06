0451cf41387a63f2e828e4336df142e972382403
Faisal Hammad <faisal_hammad@hotmail.com>
2024-06-06 17:28:53 +0200

    railway commit

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def predict():
         return jsonify({'prediction': decoded_label})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host=‘0.0.0.0’, port=os.getenv(“PORT”, default=5000))
