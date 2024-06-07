a1075f021571e0ef268c1cceae69e582db17018f
Faisal Hammad <faisal_hammad@hotmail.com>
2024-06-07 09:26:22 +0200

    Revert Flask app changes and update Docker setup

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn --chdir /app app:app
+web: gunicorn app:app
+
--- start.sh
+++ start.sh
@@ -4,5 +4,4 @@
 tensorflow_model_server --rest_api_port=8501 --model_name=project1 --model_base_path=/models/project1 &
 
 # Start Flask app
-cd /app
 gunicorn app:app --bind 0.0.0.0:5000 --workers 1
